2a5e0469bd79d637c1ed435616afce0219246a55
Sefa Eyer <eyersefa@gmail.com>
2023-03-30 21:31:13 +0300

    v1.1.5

--- src/test/java/day07_zz_Tekrar/C06_ClassWork2.java
+++ src/test/java/day07_zz_Tekrar/C06_ClassWork2.java
@@ -12,19 +12,12 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
-public class C05_ClassWork {
-    /*
-        Verilen web sayfasına gidin.
-        https://the-internet.herokuapp.com/checkboxes
-        Checkbox1 ve checkbox2 elementlerini locate edin.
-        Checkbox1 seçili değilse onay kutusunu tıklayın
-        Checkbox2 seçili değilse onay kutusunu tıklayın
-     */
+public class C06_ClassWork2 {
     WebDriver driver;
     @Before
     public void setUp() throws Exception {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
@@ -37,15 +30,19 @@ public class C05_ClassWork {
 
     @Test
     public void test01() {
+        //        Verilen web sayfasına gidin.
         //        https://the-internet.herokuapp.com/checkboxes
         driver.get("https://the-internet.herokuapp.com/checkboxes");
+
         //        Checkbox1 ve checkbox2 elementlerini locate edin.
-        WebElement cb1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
-        WebElement cb2 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
+        WebElement cb1 = driver.findElement(By.xpath("(//*[@type='checkbox'])[1]"));
+        WebElement cb2 = driver.findElement(By.xpath("(//*[@type='checkbox'])[2]"));
+
         //        Checkbox1 seçili değilse onay kutusunu tıklayın
         if(!cb1.isSelected()){
             cb1.click();
         }
+
         //        Checkbox2 seçili değilse onay kutusunu tıklayın
         if(!cb2.isSelected()){
             cb2.click();
